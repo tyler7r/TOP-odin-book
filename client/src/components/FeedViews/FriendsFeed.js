1f514525442bdc04c8962bc16bcb48a8c41b4fbc
Tyler Randall <tyler7r@gmail.com>
2023-07-13 15:00:37 -0400

    Refactor code among home page views

--- client/src/components/FeedViews/FriendsFeed.js
+++ client/src/components/FeedViews/FriendsFeed.js
@@ -5,33 +5,10 @@ import { DisplayPosts } from '../DisplayPosts';
 import '../Home.css';
 
 export const FriendsFeed = (props) => {
-    const { user, token, view, setView } = props;
+    const { user, token, fetchPosts } = props;
     const [posts, setPosts] = useState(null);
     const [skip, setSkip] = useState(0);
     // const [errors, setErrors] = useState(null);
-    
-    const fetchPosts = async () => {
-        try {
-            await fetch(`/odinbook?skip=${skip}&feed=${view}`, {
-                method: 'get',
-                headers: {
-                    'Authorization': token,
-                    'Content-Type': 'application/json',
-                },
-            }).then(res => res.json())
-                .then(data => {
-                    if (posts !== null) {
-                        let friendPosts = data.posts.filter(post => data.friends.includes(post.author._id))
-                        setPosts([...posts, ...friendPosts])
-                    } else {
-                        let friendPosts = data.posts.filter(post => data.friends.includes(post.author._id))
-                        setPosts(friendPosts);
-                    }
-                })
-        } catch (err){
-            console.log(err)
-        }
-    }
 
     useEffect(() => {
         if (token !== null) {
@@ -55,17 +32,6 @@ export const FriendsFeed = (props) => {
         <>
             {token !== null &&
                 <>
-                    <Link to='/odinbook/users/index'>User Index</Link>
-                    <h1>Home Page</h1>
-                    {user !== null &&
-                        <>
-                            <Link to={`/odinbook${user.url}`}>{user.fullName} @{user.username}</Link>
-                            <NewPost token={token} user={user} posts={posts} setPosts={setPosts} />
-                        </>
-                    }
-                    <button onClick={() => setView('friends')}>Friends</button>
-                    <button onClick={() => setView('recent')}>Recent</button>
-                    <button onClick={() => setView('popular')}>Popular</button>
                     <div className='feed' onScroll={handleScroll}>
                         <h4>Friends Feed</h4>
                         {(posts !== null && posts.length !== 0)
--- client/src/components/FeedViews/RecentFeed.js
+++ client/src/components/FeedViews/RecentFeed.js
@@ -5,31 +5,10 @@ import { DisplayPosts } from '../DisplayPosts';
 import '../Home.css'
 
 export const RecentFeed = (props) => {
-    const { user, token, setView} = props;
+    const { user, token, fetchPosts } = props;
     const [posts, setPosts] = useState(null);
     const [skip, setSkip] = useState(0);
     // const [errors, setErrors] = useState(null);
-    
-    const fetchPosts = async () => {
-        try {
-            await fetch(`/odinbook?skip=${skip}`, {
-                method: 'get',
-                headers: {
-                    'Authorization': token,
-                    'Content-Type': 'application/json',
-                },
-            }).then(res => res.json())
-                .then(data => {
-                    if (posts !== null) {
-                        setPosts([...posts, ...data.posts])
-                    } else {
-                        setPosts(data.posts)
-                    }
-                })
-        } catch (err){
-            console.log(err)
-        }
-    }
 
     useEffect(() => {
         if (token !== null) {
@@ -53,17 +32,6 @@ export const RecentFeed = (props) => {
         <>
             {token !== null &&
                 <>
-                    <Link to='/odinbook/users/index'>User Index</Link>
-                    <h1>Home Page</h1>
-                    {user !== null &&
-                        <>
-                            <Link to={`/odinbook${user.url}`}>{user.fullName} @{user.username}</Link>
-                            <NewPost token={token} user={user} posts={posts} setPosts={setPosts} />
-                        </>
-                    }   
-                    <button onClick={() => setView('friends')}>Friends</button>
-                    <button onClick={() => setView('recent')}>Recent</button>
-                    <button onClick={() => setView('popular')}>Popular</button>
                     <div className='feed' onScroll={handleScroll}>
                         <h4>Recent Feed</h4>
                         {(posts !== null && posts.length !== 0)
